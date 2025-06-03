c27523c9a1e7f1de781c8b4f34edaedb5860791c
Margarita Kikot <ritkaplitka2003@mail.ru>
2025-06-03 23:10:39 +1000

    refactor base models

---
+++ models/pirate_net.py
@@ -0,0 +1,35 @@
+from torch import nn
+from .utils.base_classes import BasePINN
+from .utils.losses import PINN_Loss
+
+class PirateNetPINN(BasePINN):
+    def __init__(
+        self,
+        input_dim,
+        output_dim,
+        hidden_layers,
+        equation,
+        loss_class=PINN_Loss,
+        activation=nn.Tanh(),
+        scaling_function=None,
+        rff_features=0,
+        rff_sigma=1.0,
+        seed=None,
+        num_blocks=3,
+        loss_kwargs=None
+    ):
+        model_kwargs = {"num_blocks" : num_blocks}
+        super().__init__(
+            input_dim=input_dim,
+            output_dim=output_dim,
+            hidden_layers=hidden_layers,
+            equation=equation,
+            loss_class=loss_class,
+            activation=activation,
+            scaling_function=scaling_function,
+            rff_features=rff_features,
+            rff_sigma=rff_sigma,
+            seed=seed,
+            model_kwargs=model_kwargs,
+            loss_kwargs=loss_kwargs
+        )
